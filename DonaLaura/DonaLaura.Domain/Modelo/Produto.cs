8650f45c3fe3bf92894f160cb383410a65f959d5
unknown <johnson9697@hotmail.com>
2018-05-12 14:59:55 -0300

    Commit Inicial

---
+++ DonaLaura/DonaLaura.Domain/Modelo/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DonaLaura.Domain.Modelo
+{
+    public class Produto : Entidade
+    {
+        public override void Validacoes()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
