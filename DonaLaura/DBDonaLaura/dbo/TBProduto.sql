835b0244cf29ba947254457ebf1792ec319ca6bc
unknown <johnson9697@hotmail.com>
2018-05-13 19:13:35 -0300

    Criação do projeto DB de Produto

---
+++ DonaLaura/DBDonaLaura/dbo/TBProduto.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[TBProduto]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Nome] VARCHAR(50) NULL, 
+    [PrecoVenda] NUMERIC(10, 2) NULL, 
+    [PrecoCusto] NUMERIC(10, 2) NULL, 
+    [Disponibilidade] BIT NULL, 
+    [DataFabricacao] DATE NULL, 
+    [DataValidade] DATE NULL
+)
