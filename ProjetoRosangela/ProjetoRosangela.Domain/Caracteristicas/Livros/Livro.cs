c7af02620ea6ebfffb18fcd2f2fb2d172ebed0c2
unknown <johnson9697@hotmail.com>
2018-05-23 20:24:42 -0300

    Projeto Rosangela primeiro commit

---
+++ ProjetoRosangela/ProjetoRosangela.Domain/Caracteristicas/Livros/Livro.cs
@@ -0,0 +1,21 @@
+﻿using ProjetoRosangela.Domain.Caracteristicas.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoRosangela.Domain.Caracteristicas.Livros
+{
+    public class Livro : Entidade
+    {
+        public string Titulo { get; set; }
+        public string Tema { get; set; }
+        public string Autor { get; set; }
+
+        public override void Validar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProjetoRosangela/ProjetoRosangela/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProjetoRosangela")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProjetoRosangela")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6e8aeff2-eaf0-49dc-8ed5-e883c7ddc6ad")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
